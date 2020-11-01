de8e4212261b58eaa07259dd7daf0773d1dfc501
patkub2 <patrykkubala1999@gmail.com>
2020-11-01 20:05:55 +0100

    added reading from file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
-
-using namespace std;
 
 int main()
 {
 
-  
-    return 0;
+    std::ifstream f("input.txt");
+    std::string line;
+
+    while (std::getline(f, line))
+    {
+        std::string gate;
+        int a, b;
+
+        char dash; // to absorb the '-' separator
+
+        std::istringstream ss(line);
+
+        ss >> gate >> a >> b;
+
+        if (gate == "OUT:")
+        {
+            std::cout << gate << "\t" << a << "\n";
+        }
+        else
+
+            std::cout << gate << "\t" << a << "\t" << b << "\n";
+    }
 }
