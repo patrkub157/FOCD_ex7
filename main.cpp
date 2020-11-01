2ca39eeaf656799f5309b5dfde42af60c17cc4ee
patkub2 <patrykkubala1999@gmail.com>
2020-11-01 20:32:03 +0100

    working reading from file pre tab

--- main.cpp
+++ main.cpp
@@ -12,20 +12,25 @@ int main()
     while (std::getline(f, line))
     {
         std::string gate;
-        int a, b;
+        int a, b, result, end;
 
         char dash; // to absorb the '-' separator
 
         std::istringstream ss(line);
 
-        ss >> gate >> a >> b;
+        ss >> gate >> a >> b >> result;
 
-        if (gate == "OUT:")
+        if (gate == "IN:")
         {
-            std::cout << gate << "\t" << a << "\n";
+            std::cout << gate << "\t" << a << "\t" << b << "\n";
+        }
+        else if (gate == "OUT:")
+        {
+            end = a;
+            std::cout << gate << "\t" << end << "\n";
         }
         else
-
-            std::cout << gate << "\t" << a << "\t" << b << "\n";
+            ////////////////////////////////////
+            std::cout << gate << "\t" << a << "\t" << b << "\t" << result << "\n";
     }
 }
